204d49985421e2bfe017592f2eb0a781d4b13b14
gusgh9176 <gusgh9176@naver.com>
2020-01-08 16:56:50 +0900

    init design

--- src/main/resources/static/js/app/main.js
+++ src/main/resources/static/js/app/main.js
@@ -1,8 +1,37 @@
 var main = {
     init : function () {
         var _this = this;
+        var id = $('.classification').length;
+        $('tbody').hide();
+        $('#tbodySkeleton').show();
+        $('.tbody-purchaseList').show();
+
         $('#btn-save').on('click', function () {
             _this.save();
+        });
+
+        $('#btn-skeleton').on('click', function () {
+            _this.skeletonNext();
+        });
+        $('#btn-insulation').on('click', function () {
+            _this.insulationNext();
+        });
+        $('#btn-exterior').on('click', function () {
+            _this.exteriorNext();
+        });
+        $('#btn-interior').on('click', function () {
+            _this.interiorNext();
+        });
+        $('#btn-window').on('click', function () {
+            _this.windowNext();
+        });
+
+
+        $('#btn-test').on('click',function () {
+            _this.test();
+        });
+        $('#btn-test2').on('click',function () {
+            _this.test2();
         });
     },
     save : function () {
@@ -24,6 +53,39 @@ var main = {
         }).fail(function (error) {
             alert(error);
         });
+    },
+
+    skeletonNext : function () {
+        $('#tbodySkeleton').hide();
+        $('#tbodyInsulation').show();
+    },
+    insulationNext : function () {
+        $('#tbodyInsulation').hide();
+        $('#tbodyExterior').show();
+    },
+    exteriorNext : function () {
+        $('#tbodyExterior').hide();
+        $('#tbodyInterior').show();
+    },
+    interiorNext : function () {
+        $('#tbodyInterior').hide();
+        $('#tbodyWindow').show();
+    },
+    windowNext : function () {
+        $('#tbodyWindow').hide();
+        $('#tbodySkeleton').show();
+        alert("선택항목이 구매리스트에 올라갔습니다.")
+    },
+
+    addPurchaseList : function () {
+
+    },
+
+    test : function () {
+        $('#tbodySkeleton').show();
+    },
+    test2 : function () {
+        $('#tbodySkeleton').hide();
     }
 
 };
