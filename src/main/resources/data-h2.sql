a9cbcdf713e322e28ade0936ce13bf1ee89db36e
gusgh9176 <gusgh9176@naver.com>
2020-01-09 15:29:30 +0900

    Test sql

--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -1,1 +1,1 @@
-insert into materials (classification, item, standard, price, created_date, modified_date) values ('테스트1', 'test1@gmail.com', '테스트1의 본문', '10,000원', now(), now());
+insert into materials (classification, item, standard, price, created_date, modified_date) values ('골조', '테스트품목', '규격', '10,000원', now(), now());
