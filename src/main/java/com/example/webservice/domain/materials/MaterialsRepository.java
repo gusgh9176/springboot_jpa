8d538b00dd7cc58bc9e7da497677e1446418e538
gusgh9176 <gusgh9176@naver.com>
2020-01-20 15:55:24 +0900

    변경 find구조
    각 품목별로 메소드가 나눠져 있던 것을 findByClassification으로 통합

--- src/main/java/com/example/webservice/domain/materials/MaterialsRepository.java
+++ src/main/java/com/example/webservice/domain/materials/MaterialsRepository.java
@@ -6,33 +6,13 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.stream.Stream;
 
 public interface MaterialsRepository extends JpaRepository<Materials, Long> {
+
     @Query("SELECT m " +
             "FROM Materials m " +
             "ORDER BY m.id DESC")
     Stream<Materials> findAllDesc();
 
-    @Query("SELECT m " +
-            "FROM Materials m " +
-            "WHERE m.classification LIKE '%골조%'")
-    Stream<Materials> findSkeleton();
+    Stream<Materials> findByClassification(String material);
 
-    @Query("SELECT m " +
-            "FROM Materials m " +
-            "WHERE m.classification LIKE '%단열%'")
-    Stream<Materials> findInsulation();
 
-    @Query("SELECT m " +
-            "FROM Materials m " +
-            "WHERE m.classification LIKE '%외장마감%'")
-    Stream<Materials> findExterior();
-
-    @Query("SELECT m " +
-            "FROM Materials m " +
-            "WHERE m.classification LIKE '%내장마감%'")
-    Stream<Materials> findInterior();
-
-    @Query("SELECT m " +
-            "FROM Materials m " +
-            "WHERE m.classification LIKE '%창호%'")
-    Stream<Materials> findWindow();
 }
